8c37c84e3d72c7eebf59202137c4841e0e657f21
_ <_>
2021-12-13 13:27:40 +0000

    added signout button to navbar

--- medplus/frontend/src/Components/NavBar/index.js
+++ medplus/frontend/src/Components/NavBar/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link, Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import { HiHome } from 'react-icons/hi';
 import * as ROUTES from '../../constants/routes';
+import { withFirebase } from '../Firebase';
 
-function NavBar() {
+function NavBar({ firebase }) {
     return (
         <nav className="nav-bar">
-            <Link to={ROUTES.HOME} className="font-medium">
-                Reminders
+            <Link to={ROUTES.LANDING} onClick={firebase.doSignOut} className="font-medium">
+                Sign Out
             </Link>
             <Link to={ROUTES.HOME} className="font-medium">
                 <HiHome />
@@ -19,4 +20,4 @@ function NavBar() {
     );
 }
 
-export default NavBar;
+export default withFirebase(NavBar);
