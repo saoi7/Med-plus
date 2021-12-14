ca564f779ee4114b225d64dc005bb09a1247352c
Darren <darren.delaney.2018@mumail.ie>
2021-12-14 18:13:58 +0000

    added MedPlus title to landing page

--- medplus/frontend/src/Components/LandingPage/index.js
+++ medplus/frontend/src/Components/LandingPage/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Link, Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import SignInPage from '../SignIn';
+import { CgPill } from 'react-icons/cg';
 
 function LandingPage() {
     return (
         <div className="background-image landing-layout">
+            <div className="landing-title font-very-large">
+                MedPlus<span style={{marginLeft: 20}}><CgPill /></span>
+            </div>
             <div className="button-container flex-container flex-justify-content-space-around">
                 <Link to={ROUTES.SIGN_IN} className="link-button background-blue background-blue-hover font-small">
                     LOGIN
